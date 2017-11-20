8a3ab8ee6c446595e524bfb878f907d060e95d3a
vadimDol <vadim.dolganof@gmail.com>
2017-11-20 19:58:09 +0300

    описал модель с данными пользователя и добавил таблицу, в качестве ORM использовал propel

---
+++ data/sql/lib.model.schema.sql
@@ -0,0 +1,26 @@
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+#-----------------------------------------------------------------------------
+#-- user
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+
+CREATE TABLE `user`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`login` VARCHAR(50)  NOT NULL,
+	`password` VARCHAR(100)  NOT NULL,
+	`name` VARCHAR(40)  NOT NULL,
+	`last_name` VARCHAR(50)  NOT NULL,
+	`role` VARCHAR(10)  NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `user_U_1` (`login`)
+)Engine=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
