194040d6bad0506ece9ef7b27467386cc2a5fe79
vadimDol <vadim.dolganof@gmail.com>
2017-11-30 01:53:22 +0300

    Доработал вид url

--- apps/frontend/modules/user/templates/indexSuccess.php
+++
@@ -1,5 +0,0 @@
-<a href="<?= url_for('user/sign_in') ?>"><h1>Sign In</h1></a>
-
-<a href="<?= url_for('user/user_list') ?>"><h1>User List</h1></a>
-
-<a href="<?= url_for('hello_world/index') ?>"><h1>Hello World</h1></a>
