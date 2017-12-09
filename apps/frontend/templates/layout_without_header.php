214ba3c5448c6749f3bd26675fb06e92ca8d8910
vadimDol <vadim.dolganof@gmail.com>
2017-12-10 00:22:09 +0300

    Вынес в отдельные файлы общий html код из layout.php, layout_without_header.php

--- apps/frontend/templates/layout_without_header.php
+++ apps/frontend/templates/layout_without_header.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-    <head>
-        <?
-        include_http_metas();
-        include_metas();
-        include_title();
-        include_stylesheets();
-        include_javascripts();
-        ?>
-        <link rel="shortcut icon" href="/favicon.ico" />
-    </head>
+    <? include_partial('global/head') ?>
     <body>
-        <div class="main">
-            <?php echo $sf_content ?>
-        </div>
+        <? include_partial('global/main_container', array('content' => $sf_content)) ?>
     </body>
 </html>
