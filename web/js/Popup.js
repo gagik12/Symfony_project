eb9b42fbcb38abb752ebbc426a69015bc04d9148
vadimDol <vadim.dolganof@gmail.com>
2018-01-05 23:36:50 +0300

    Рефакторинг js кода

--- web/js/Popup.js
+++ web/js/Popup.js
@@ -2,16 +2,21 @@ function Popup()
 {
     const BOX_WIDTH = 400;
 
+    this.popupLink = $('.popup-link');
+    this.popupBox = $('.popup-box');
+    this.cover = $('.cover');
+
+
     this.setEvents = function() {
         var self = this;
-        $('.popup-link').click(function(e) {
+        this.popupLink.click(function(e) {
             e.preventDefault();
             e.stopPropagation();
 
             self.openBox();
         });
         //при нажатии на box, popup не закрывается
-        $('.popup-box').click(function(e) {
+        this.popupBox.click(function(e) {
             e.stopPropagation();
         });
 
@@ -23,15 +28,15 @@ function Popup()
     this.openBox = function() {
         this.setCenterBox();
 
-        $('.popup-box').show();
-        $('.cover').show();
+        this.popupBox.show();
+        this.cover.show();
         $('html, body').css('overflow', 'hidden');
     };
 
     this.closeBox = function() {
-        $('.popup-box').hide();
-        $('.cover').hide();
-        $("html,body").css("overflow", "auto");
+        this.popupBox.hide();
+        this.cover.hide();
+        $("html, body").css("overflow", "auto");
     };
 
     this.setCenterBox = function() {
@@ -43,6 +48,6 @@ function Popup()
         var boxPositionX = (documentWidth - BOX_WIDTH) / 2;
         var boxPositionY = scrollPosition + OFFSET_TOP;
 
-        $('.popup-box').css({'width': BOX_WIDTH + 'px', 'left': boxPositionX + 'px', 'top': boxPositionY + 'px'});
+        this.popupBox.css({'width': BOX_WIDTH + 'px', 'left': boxPositionX + 'px', 'top': boxPositionY + 'px'});
     };
 }
